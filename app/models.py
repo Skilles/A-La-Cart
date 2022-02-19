72a871cb85ba987a16cd6debf4cd811d0c8a5e95
Skilles <haloeer1@gmail.com>
2022-02-18 21:03:04 -0500

    Base A La Cart changes

--- app/models.py
+++ app/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 
 # Create your models here.
-class Greeting(models.Model):
-    when = models.DateTimeField("date created", auto_now_add=True)
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
--- app/views.py
+++ app/views.py
@@ -1,19 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-from .models import Greeting
+from .models import Product
 
 # Create your views here.
 def index(request):
     # return HttpResponse('Hello from Python!')
     return render(request, "index.html")
 
-
 def db(request):
-
-    greeting = Greeting()
-    greeting.save()
-
-    greetings = Greeting.objects.all()
-
-    return render(request, "db.html", {"greetings": greetings})
+    # Display every product in the database
+    products = Product.objects.all()
+    return render(request, "db.html", {'products': products})
